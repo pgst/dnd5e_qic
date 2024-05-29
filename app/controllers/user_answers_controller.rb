45051611299e7ef34cc00a5be3efca5086f603fa
Shinichi TAKATSUKA <pgstaka@gmail.com>
2024-05-29 15:18:16 +0900

    回答欄レコードを用意する処理

--- app/controllers/user_answers_controller.rb
+++ app/controllers/user_answers_controller.rb
@@ -1,5 +1,10 @@
 class UserAnswersController < ApplicationController
+  # 回答欄テーブルのデータ作成準備
   def new
+    # 試験問題テーブルの使用可能な全問題数を取得
+    @examination_count = Examination.where(can_use: true).pluck(:id).size
+    # 問題数の初期値を設定
+    @question_num_all = 10
   end
 
   def create
