57dfbb4258cdfba9e3a3b1cbe0b3b5909a660f8f
Shinichi TAKATSUKA <pgstaka@gmail.com>
2024-05-29 13:53:38 +0900

    Applicationコントローラにログイン関連メソッドを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  include SessionsHelper
+  before_action :check_logged_in
+
+  def check_logged_in
+    return if current_user
+
+    flash[:error] = 'ログインしてください'
+
+    redirect_to root_path
+  end
 end
