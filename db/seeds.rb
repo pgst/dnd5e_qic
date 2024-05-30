25ab6e52b471d2dd2a9ec70db75b37eaca56a855
Shinichi TAKATSUKA <pgstaka@gmail.com>
2024-05-30 11:10:06 +0900

    バックアップCSVファイルからDBに保存する処理を追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,45 @@
 require 'csv'
 
-csv = CSV.read('db/examinations.csv', headers: false)
+examinations_csv = CSV.read('db/examinations.csv', headers: false)
+examinations_csv.each do |row|
+  can_use = row[3] == 'true' ? true : false
 
-csv.each do |row|
   Examination.create!(
-    question_txt: row[0],
-    correct_ans: row[1],
-    can_use: true
+    id: row[0],
+    question_txt: row[1],
+    correct_ans: row[2],
+    can_use: can_use
+  )
+end
+
+users_csv = CSV.read('db/users.csv', headers: false)
+users_csv.each do |row|
+  admin = row[5] == 'true' ? true : false
+
+  User.create!(
+    id: row[0],
+    name: row[1],
+    image: row[2],
+    uid: row[3],
+    passed_num: row[4],
+    admin: admin
+  )
+end
+
+user_answers_csv = CSV.read('db/user_answers.csv', headers: false)
+user_answers_csv.each do |row|
+  choiced_ans = row[1] == ' - ' ? nil : row[1]
+  cleared = row[2] == 'true' ? true : false
+
+  UserAnswer.create!(
+    id: row[0],
+    choiced_ans: choiced_ans,
+    cleared: cleared,
+    attempts_num: row[3],
+    question_num: row[4],
+    created_at: row[5],
+    updated_at: row[6],
+    user_id: row[7],
+    examination_id: row[8]
   )
 end
