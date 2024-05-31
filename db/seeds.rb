d0c84687f85ff99be1e0e907117d37f9af2679e4
Shinichi TAKATSUKA <pgstaka@gmail.com>
2024-05-31 10:38:37 +0900

    回答欄データバックアップからの処理を変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,6 @@
 require 'csv'
 
+# 試験問題データを取り込む
 examinations_csv = CSV.read('db/examinations.csv', headers: true)
 examinations_csv.each do |row|
   can_use = row[3] == 'true' ? true : false
@@ -16,6 +17,7 @@ examinations_csv.each do |row|
   )
 end
 
+# ユーザーデータを取り込む
 users_csv = CSV.read('db/users.csv', headers: true)
 users_csv.each do |row|
   admin = row[5] == 'true' ? true : false
@@ -34,10 +36,13 @@ users_csv.each do |row|
   )
 end
 
-user_answers_csv = CSV.read('db/user_answers.csv', headers: false)
+# 回答欄データを取り込む
+user_answers_csv = CSV.read('db/user_answers.csv', headers: true)
 user_answers_csv.each do |row|
   choiced_ans = row[1] == ' - ' ? nil : row[1]
   cleared = row[2] == 'true' ? true : false
+  created_at = row[6] == '' ? Time.current : row[5]
+  updated_at = row[7] == '' ? Time.current : row[6]
 
   UserAnswer.create!(
     id: row[0],
@@ -45,8 +50,8 @@ user_answers_csv.each do |row|
     cleared: cleared,
     attempts_num: row[3],
     question_num: row[4],
-    created_at: row[5],
-    updated_at: row[6],
+    created_at: created_at,
+    updated_at: updated_at,
     user_id: row[7],
     examination_id: row[8]
   )
