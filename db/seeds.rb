b017a1131ffe80b1b82d074c5fabbc84a05ace97
Shinichi TAKATSUKA <pgstaka@gmail.com>
2024-05-31 10:43:12 +0900

    CSVファイルが見つからない場合の処理を追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,58 +1,70 @@
 require 'csv'
 
-# 試験問題データを取り込む
-examinations_csv = CSV.read('db/examinations.csv', headers: true)
-examinations_csv.each do |row|
-  can_use = row[3] == 'true' ? true : false
-  created_at = row[4] == '' ? Time.current : row[4]
-  updated_at = row[5] == '' ? Time.current : row[5]
+begin
+  examinations_csv = CSV.read('db/examinations.csv', headers: true)
+  examinations_csv.each do |row|
+    can_use = row[3] == 'true' ? true : false
+    created_at = row[4] == '' ? Time.current : row[4]
+    updated_at = row[5] == '' ? Time.current : row[5]
 
-  Examination.create!(
-    id: row[0],
-    question_txt: row[1],
-    correct_ans: row[2],
-    can_use: can_use,
-    created_at: created_at,
-    updated_at: updated_at
-  )
+    Examination.create!(
+      id: row[0],
+      question_txt: row[1],
+      correct_ans: row[2],
+      can_use: can_use,
+      created_at: created_at,
+      updated_at: updated_at
+    )
+  end
+rescue Errno::ENOENT
+  # ファイルが見つからない場合の処理
+  Rails.logger.debug 'examinations.csv not found'
 end
 
-# ユーザーデータを取り込む
-users_csv = CSV.read('db/users.csv', headers: true)
-users_csv.each do |row|
-  admin = row[5] == 'true' ? true : false
-  created_at = row[6] == '' ? Time.current : row[6]
-  updated_at = row[7] == '' ? Time.current : row[7]
+begin
+  users_csv = CSV.read('db/users.csv', headers: true)
+  users_csv.each do |row|
+    admin = row[5] == 'true' ? true : false
+    created_at = row[6] == '' ? Time.current : row[6]
+    updated_at = row[7] == '' ? Time.current : row[7]
 
-  User.create!(
-    id: row[0],
-    name: row[1],
-    image: row[2],
-    uid: row[3],
-    passed_num: row[4],
-    admin: admin,
-    created_at: created_at,
-    updated_at: updated_at
-  )
+    User.create!(
+      id: row[0],
+      name: row[1],
+      image: row[2],
+      uid: row[3],
+      passed_num: row[4],
+      admin: admin,
+      created_at: created_at,
+      updated_at: updated_at
+    )
+  end
+rescue Errno::ENOENT
+  # ファイルが見つからない場合の処理
+  Rails.logger.debug 'users.csv not found'
 end
 
-# 回答欄データを取り込む
-user_answers_csv = CSV.read('db/user_answers.csv', headers: true)
-user_answers_csv.each do |row|
-  choiced_ans = row[1] == ' - ' ? nil : row[1]
-  cleared = row[2] == 'true' ? true : false
-  created_at = row[6] == '' ? Time.current : row[5]
-  updated_at = row[7] == '' ? Time.current : row[6]
+begin
+  user_answers_csv = CSV.read('db/user_answers.csv', headers: true)
+  user_answers_csv.each do |row|
+    choiced_ans = row[1] == ' - ' ? nil : row[1]
+    cleared = row[2] == 'true' ? true : false
+    created_at = row[6] == '' ? Time.current : row[5]
+    updated_at = row[7] == '' ? Time.current : row[6]
 
-  UserAnswer.create!(
-    id: row[0],
-    choiced_ans: choiced_ans,
-    cleared: cleared,
-    attempts_num: row[3],
-    question_num: row[4],
-    created_at: created_at,
-    updated_at: updated_at,
-    user_id: row[7],
-    examination_id: row[8]
-  )
+    UserAnswer.create!(
+      id: row[0],
+      choiced_ans: choiced_ans,
+      cleared: cleared,
+      attempts_num: row[3],
+      question_num: row[4],
+      created_at: created_at,
+      updated_at: updated_at,
+      user_id: row[7],
+      examination_id: row[8]
+    )
+  end
+rescue Errno::ENOENT
+  # ファイルが見つからない場合の処理
+  Rails.logger.debug 'user_answers.csv not found'
 end
