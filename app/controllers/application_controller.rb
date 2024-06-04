0c81fec5aab327708e6937fff7cece4cb92187e7
Shinichi TAKATSUKA <pgstaka@gmail.com>
2024-06-04 11:41:28 +0900

    rescue_from追加、record_not_foundメソッドのみ

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,17 @@
 class ApplicationController < ActionController::Base
   include SessionsHelper
   before_action :check_logged_in
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
   def check_logged_in
     return if current_user
     flash[:error] = 'ログインしてください'
     redirect_to root_path
+  end
+
+  private
+
+  def record_not_found
+    render plain: "404 Not Found", status: 404
   end
 end
