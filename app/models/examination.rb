e4f5ca52e0f2babd8ecb0b55d09d7ae5f70f4283
Shinichi TAKATSUKA <pgstaka@gmail.com>
2024-06-04 17:50:18 +0900

    使用していないscopeを削除

--- app/models/examination.rb
+++ app/models/examination.rb
@@ -3,8 +3,6 @@ class Examination < ApplicationRecord
 
   # 使用可能な全問題数を取得
   scope :get_examination_count, -> { where(can_use: true).pluck(:id).size }
-  # 使用可能な全問題データを取得
-  scope :get_examination_all, -> { where(can_use: true).all }
   # 使用可能な全問題データのIDをランダムに取得
   scope :get_examination_ids_rand, -> (question_num_all) { where(can_use: true).order("RANDOM()").limit(question_num_all).pluck(:id) }
 end
